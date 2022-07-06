b6cb381f7e445191d402beae883311e535f3d93a
Dionysus <dionysus.cho@gmail.com>
2022-07-06 08:46:43 -0700

    setup with skr mini

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1283,7 +1283,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 40, 15, -0.1 }
+#define NOZZLE_TO_PROBE_OFFSET { 40, 15, -1.9 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1495,11 +1495,11 @@
 
 // The size of the printable area
 #define X_BED_SIZE 296
-#define Y_BED_SIZE 220
+#define Y_BED_SIZE 200
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -24
+#define Y_MIN_POS -22
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
